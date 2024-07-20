840ee70e3ed8189abcba9da07cac89b38f5885cc
mishraprajwal <prajwalm882@gmail.com>
2024-07-20 17:51:58 -0400

    Update .gitignore to ignore node_modules, .env, package-lock.json, and dist

--- backend/dist/app.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { config } from "dotenv";
-config();
-const app = express();
-app.use(express.json());
-export default app;
-//# sourceMappingURL=app.js.map
--- backend/dist/index.js
+++
@@ -1,7 +0,0 @@
-import app from "./app.js";
-import { connectToDatabase } from "./db/connection.js";
-const PORT = process.env.PORT || 5001;
-connectToDatabase().then(() => {
-    app.listen(5001, () => console.log("Server Open and connected to database."));
-}).catch((err) => console.log(err));
-//# sourceMappingURL=index.js.map
