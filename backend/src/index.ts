981e075947f09694f2818f0b51260c5c72480444
mishraprajwal <prajwalm882@gmail.com>
2024-07-20 15:56:07 -0400

    setup Node.js, Express and integrated MongoDB

---
+++ backend/src/index.ts
@@ -0,0 +1,7 @@
+import app from "./app.js";
+import { connectToDatabase } from "./db/connection.js";
+
+const PORT = process.env.PORT || 5001;
+connectToDatabase(). then(() => {
+    app.listen(5001, () => console.log("Server Open and connected to database."));
+}).catch((err) => console.log(err));
